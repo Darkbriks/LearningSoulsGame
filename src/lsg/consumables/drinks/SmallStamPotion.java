8b1351c20eb91034ca5bd0541d69a4d48c46b53e
Darkbriks <antoine.patron.62610@gmail.com>
2024-02-16 09:36:33 +0100

    11.1 Add consumables (SuperBerry.java and SmallStamPotion.java)

---
+++ src/lsg/consumables/drinks/SmallStamPotion.java
@@ -0,0 +1,6 @@
+package lsg.consumables.drinks;
+
+public class SmallStamPotion extends Drink
+{
+    public SmallStamPotion() { super("Small Stamina Potion", 20); }
+}
---
+++ src/lsg/consumables/food/SuperBerry.java
@@ -0,0 +1,6 @@
+package lsg.consumables.food;
+
+public class SuperBerry extends Food
+{
+    public SuperBerry() { super("Super Berry", 1000); }
+}
